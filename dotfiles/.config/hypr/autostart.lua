e5572c7dbdc292d5059849e1382e31d6209b2433
jonas-backup <jonas-backup@fedora.fritz.box>
2026-08-23 14:57:46 +0200

    removed unneccessary comments

--- dotfiles/.config/hypr/autostart.lua
+++ dotfiles/.config/hypr/autostart.lua
@@ -1,12 +1,8 @@
--- Autostart processes when Hyprland launches
 hl.on("hyprland.start", function()
-	-- 1. Fire up your status bar
 	hl.exec_cmd("waybar")
 
-	-- 2. Start the swww wallpaper background daemon
 	hl.exec_cmd("swww-daemon")
 
-	-- 3. Tell waypaper to automatically re-apply your last clicked wallpaper
 	hl.exec_cmd("waypaper --restore")
 
 	hl.exec_cmd("ags")
